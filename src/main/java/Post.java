67abbca504430d3e80311d2610d72ebe97f141f0
Ivan Kashuba <kashubaim@gmail.com>
2023-07-12 20:12:44 +0600

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport; // поле для паспорт
+    public String patronymic; // поле для отчество
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription;
+}
